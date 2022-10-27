f8809a29a0d5a9e0a9f63928ab1191ead7487649
hgo150925 <hugo.orlando2030@gmail.com>
2022-10-27 20:22:41 -0300

    simple method

--- Simple_Method/Simple_Method/Program.cs
+++ Simple_Method/Simple_Method/Program.cs
@@ -4,6 +4,10 @@
     {
         static void Main(string[] args)
         {
+            // https://learn.microsoft.com/es-es/dotnet/csharp/methods
+            // https://www.geeksforgeeks.org/c-sharp-methods/
+
+            // Hello World
             HelloWorld();
 
             // Sum array
@@ -11,9 +15,25 @@
             Console.WriteLine(SumArray(nums));
 
             // Reverse string
-            string rs = ReverseString("Olivia");
+            string rs = ReverseString("Olivia Lund");
             Console.WriteLine(rs);
-            
+
+            // https://www.geeksforgeeks.org/c-sharp-method-overloading/
+            // La sobrecarga de métodos es la forma común de implementar polimorfismos.
+            // Es la capacidad de redefinir una función en más de una forma. 
+            // Un usuario puede implementar la sobrecarga de funciones definiendo dos o más funciones en una clase que comparten el mismo nombre
+            // C# | Sobrecarga de métodos
+
+            int res;
+            res = Add(1, 3);
+            Console.WriteLine(res);
+
+            res = Add(2, 4, 5);
+            Console.WriteLine(res);
+
+            Countries("AR", "PY");
+            Countries("UK", "DK", "US");
+
         }
 
         private static void HelloWorld()
@@ -38,6 +58,27 @@
 
             return String.Concat(messageArray);
                     
+        }
+
+        // C# | Sobrecarga de métodos
+        private static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        private static int Add(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+
+        private static void Countries(string a, string b)
+        {
+            Console.WriteLine($"{a} - {b}");
+        }
+
+        private static void Countries(string a, string b, string c)
+        {
+            Console.WriteLine($"{a} - {b} - {c}");
         }
     }
 }
