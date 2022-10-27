c7bf36741a0e3c737286080d1e64ca38a8a45362
hgo150925 <hugo.orlando2030@gmail.com>
2022-10-27 15:59:58 -0300

    for iteration statement

---
+++ For_Iteration_Statement/For_Iteration_Statement/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace For_Iteration_Statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/statements/iteration-statements#the-for-statement
+            // La declaración "for" ejecuta una sentencia o un bloque de sentencias mientras una expresión booleana específica se evalúa como true. 
+            
+            for (int i = 0; i <= 5; i++)
+            {
+                // Console.Write(i);
+                // Output: 012345
+                if (i == 3)
+                {
+                    Console.WriteLine("Found three");
+                    break;
+                }
+
+            }
+
+            for (int i = 0; i <   5; i--)
+            {
+                Console.WriteLine(i);
+            }
+
+
+        }
+    }
+}
