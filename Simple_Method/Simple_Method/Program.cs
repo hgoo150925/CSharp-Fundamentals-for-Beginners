dffa8dcd5d3d31c82def41d6a2a8c7f764170981
hgo150925 <hugo.orlando2030@gmail.com>
2022-10-27 16:11:33 -0300

    simple method

---
+++ Simple_Method/Simple_Method/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Simple_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWorld();
+
+            // Sum array
+            int[] nums = { 1, 5, 8, 25 };
+            Console.WriteLine(SumArray(nums));
+
+            // Reverse string
+            ReverseString("I live in Buenos Aires");
+            
+        }
+
+        private static void HelloWorld()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        private static int SumArray(int[] nums)
+        {
+            int sum = 0;
+            foreach (int num in nums)
+            {
+                sum += num;
+            }
+            return sum;
+        }
+
+        private static void ReverseString(string message)
+        {
+            char[] messageArray = message.ToCharArray();
+            Array.Reverse(messageArray);
+
+            foreach (char item in messageArray)
+            {
+                Console.Write(item);
+            }          
+        }
+    }
+}
