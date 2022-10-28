d74023a0273f58423dd68b60a5de4f14ab9e00e5
hgo150925 <hugo.orlando2030@gmail.com>
2022-10-27 21:18:10 -0300

    while iterator

---
+++ While_Iteration_Statement/While_Iteration_Statement/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace While_Iteration_Statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while(i < 5)
+            {
+                Console.WriteLine("while");
+                i++;
+
+                if (i >5)
+                {
+                    break;
+                }
+            }
+
+            GuessingGame();
+
+        }
+
+        private static void GuessingGame()
+        {
+            Console.Clear();
+            Console.WriteLine("Guessing game!");
+
+            Random random = new Random();
+            int randomNumber = random.Next(0, 50);
+
+            int guesses = 0;
+            bool incorrect = true;
+
+            do
+            {
+                Console.WriteLine("Guess the number between 0 and 50");
+                string result = Console.ReadLine();
+                int res = int.Parse(result);
+
+                if(res == randomNumber)
+                {
+                    incorrect = false;                    
+                } 
+                else if (res > randomNumber )
+                {
+                    incorrect = true;
+                    Console.WriteLine("choose a smaller number");
+
+                }
+                
+                else
+                {
+                    incorrect = true;
+                    Console.WriteLine("choose a higher number");
+                }
+                
+
+
+            } while (incorrect);
+            Console.WriteLine("Correct!");
+        }
+    }
+}
