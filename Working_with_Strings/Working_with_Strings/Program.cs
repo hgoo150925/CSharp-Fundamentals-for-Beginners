f1252081e84260e320aa765ab1bba9aa9cf91e00
hgo150925 <hugo.orlando2030@gmail.com>
2022-10-30 16:19:23 -0300

    string

---
+++ Working_with_Strings/Working_with_Strings/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Working_with_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString = "Go to your c:\\ drive";
+            Console.WriteLine(myString);
+
+            string country = "Argentina";
+            Console.WriteLine(country);
+            Console.ReadLine();
+        }
+    }
+}
