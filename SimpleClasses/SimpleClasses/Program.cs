927f62a54e402b5d3232d71a101717e441b9696b
hgo150925 <hugo.orlando2030@gmail.com>
2022-11-07 21:12:23 -0300

    Simple Classes

---
+++ SimpleClasses/SimpleClasses/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace SimpleClasses
+{
+    class Program
+    {
+        // https://learn.microsoft.com/es-es/dotnet/csharp/fundamentals/tutorials/classes
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
