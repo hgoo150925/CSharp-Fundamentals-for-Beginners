a7bce6f358d576fd0eaf1872cc9ae68843dc7173
hgo150925 <hugo.orlando2030@gmail.com>
2022-11-07 21:10:36 -0300

    dates and times

---
+++ DatesAndTimes/DatesAndTimes/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace DatesAndTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime myValue = DateTime.Now;
+            Console.WriteLine(myValue.ToString().Substring(0,5));
+            Console.WriteLine(myValue.ToShortDateString());
+            Console.WriteLine(myValue.ToLongDateString());
+            Console.WriteLine(myValue.ToShortTimeString());
+            Console.WriteLine(myValue.ToLongDateString());
+            Console.WriteLine(myValue.ToLongTimeString());
+            Console.WriteLine(myValue.AddDays(4).ToLongDateString());
+            Console.WriteLine(myValue.Month);
+
+            DateTime someDate = new DateTime(1955, 08, 15);
+            Console.WriteLine(someDate);
+
+            // Convertir una fecha en string
+            DateTime dateString = DateTime.Parse("14/11/2022");
+            Console.WriteLine(dateString);
+
+            // Contar el total de días a partir de una fecha dada
+            TimeSpan myAge = DateTime.Now.Subtract(someDate);
+            Console.WriteLine(myAge.TotalDays);
+
+            Console.ReadLine();
+        }
+    }
+}
