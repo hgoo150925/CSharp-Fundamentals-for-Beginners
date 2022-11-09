6f87b61319423d9f0d1a724b14bf154a299cf842
hgo150925 <hugo.orlando2030@gmail.com>
2022-11-08 23:29:12 -0300

    añadiendo transacciones

--- SimpleClasses/SimpleClasses/Program.cs
+++ SimpleClasses/SimpleClasses/Program.cs
@@ -5,14 +5,16 @@
         // https://learn.microsoft.com/es-es/dotnet/csharp/fundamentals/tutorials/classes
         static void Main(string[] args)
         {
-            var account = new BankAccount("Jonas", 1500);
+            var account = new BankAccount("Jonas", 1250);
             Console.WriteLine($"Account {account.Number} was created for {account.Owner}");
 
-            var account2 = new BankAccount("Smith", 950);
+            var account2 = new BankAccount("Smith", 1340);
             Console.WriteLine($"Account {account2.Number} was created for {account2.Owner}");
 
+            account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+            Console.WriteLine($"Make withdrawal: {account.Balance}");
             account.MakeDeposit(500, DateTime.Now, "Friend paid me back");
-            Console.WriteLine(account.Balance);
+            Console.WriteLine($"Make deposit: {account.Balance}");
             Console.ReadLine();
 
 
