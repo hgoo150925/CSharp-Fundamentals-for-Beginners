f12aedee45deb68cd197ca0560dd4323d32c9522
hgo150925 <hugo.orlando2030@gmail.com>
2022-11-08 23:44:28 -0300

    se agrega el método GetAccountHistory que crea string para el historial de transacciones

--- SimpleClasses/SimpleClasses/Program.cs
+++ SimpleClasses/SimpleClasses/Program.cs
@@ -1,38 +1,33 @@
-﻿namespace SimpleClasses
+﻿using SimpleClasses;
+
+var account = new BankAccount("Jonas", 1000);
+Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} balance.");
+
+account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+Console.WriteLine(account.Balance);
+account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
+Console.WriteLine(account.Balance);
+
+Console.WriteLine(account.GetAccountHistory());
+
+// Test that the initial balances must be positive:
+try
 {
-    class Program
-    {
-        // https://learn.microsoft.com/es-es/dotnet/csharp/fundamentals/tutorials/classes
-        static void Main(string[] args)
-        {
-            var account = new BankAccount("Jonas", 1250);
-            Console.WriteLine($"Account {account.Number} was created for {account.Owner}");
+    var invalidAccount = new BankAccount("invalid", -55);
+}
+catch (ArgumentOutOfRangeException e)
+{
+    Console.WriteLine("Exception caught creating account with negative balance");
+    Console.WriteLine(e.ToString());
+}
 
-            var account2 = new BankAccount("Smith", 1340);
-            Console.WriteLine($"Account {account2.Number} was created for {account2.Owner}");
-
-            account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
-            Console.WriteLine($"Make withdrawal: {account.Balance}");
-            account.MakeDeposit(500, DateTime.Now, "Friend paid me back");
-            Console.WriteLine($"Make deposit: {account.Balance}");
-
-            Console.WriteLine(account.GetAccountHistory());
-
-            // Test that the initial balances must be positive.
-            BankAccount invalidAccount;
-
-            // Test for a negative balance.
-            try
-            {
-                account.MakeWithdrawal(750, DateTime.Now, "Attempt to overdraw");
-            }
-            catch (InvalidOperationException e)
-            {
-                Console.WriteLine("Exception caught trying to overdraw");
-                Console.WriteLine(e.ToString());
-            }
-
-            Console.ReadLine();
-        }
-    }
+// Test for a negative balance
+try
+{
+    account.MakeWithdrawal(750, DateTime.Now, "Attempt to overdraw");
+}
+catch (InvalidOperationException e)
+{
+    Console.WriteLine("Exception caught trying to overdraw");
+    Console.WriteLine(e.ToString());
 }
