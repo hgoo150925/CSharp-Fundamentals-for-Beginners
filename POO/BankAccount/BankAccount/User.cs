f1c3abdbe7229d1ea902bd4b53448560eb958da8
hgo150925 <hugo.orlando2030@gmail.com>
2022-11-10 23:49:49 -0300

    agregando POO

---
+++ POO/BankAccount/BankAccount/User.cs
@@ -0,0 +1,11 @@
+﻿namespace BankAccount;
+
+public class User
+{
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public DateTime RegisterDate { get; set; }
+
+
+}
